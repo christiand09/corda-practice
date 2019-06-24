670ec41e12c51c784f88d32f4ea3d05f27589cb5
archie <archie@quantumcrowd.io>
2019-06-24 09:14:54 +0800

    message

--- contracts/src/main/kotlin/com/template/states/ProjState.kt
+++ contracts/src/main/kotlin/com/template/states/ProjState.kt
@@ -1,20 +1,22 @@
 package com.template.states
 
-import net.corda.core.contracts.Amount
-import net.corda.core.contracts.Contract
+
+import com.template.contracts.ProjContract
+import net.corda.core.contracts.BelongsToContract
 import net.corda.core.contracts.LinearState
 import net.corda.core.contracts.UniqueIdentifier
 import net.corda.core.identity.Party
-import java.util.*
 
-abstract class ProjState : Contract {
-
-    data class ProjState(
-            val amount: Amount<Currency>,
-            val lender: Party,
-            val borrower: Party,
-            val paid: Amount<Currency> = Amount(0, amount.token),
-            override val linearId: UniqueIdentifier = UniqueIdentifier(),
-            override val participants: List<Party> = listOf(lender, borrower)
-    ) : LinearState
-}
+@BelongsToContract(ProjContract::class)
+data class ProjState(
+        val firstName: String,
+        val lastName: String,
+        val age: Int,
+        val gender: String,
+        val address: String,
+        val isApproved: Boolean,
+        val unRegistered: Party,
+        val toRegister: Party,
+        override val linearId: UniqueIdentifier = UniqueIdentifier(),
+        override val participants: List<Party> = listOf(unRegistered,toRegister)
+) : LinearState
