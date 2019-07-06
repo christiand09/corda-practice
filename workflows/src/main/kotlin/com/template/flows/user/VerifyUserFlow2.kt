465add5870d8ffa430a901d8a00bce0f67c38e57
Lance Navarro <lancegbn@gmail.com>
2019-07-06 14:13:32 +0800

    working verifyuser2. send to all parties

---
+++ workflows/src/main/kotlin/com/template/flows/user/VerifyUserFlow2.kt
@@ -0,0 +1,59 @@
+package com.template.flows.user
+
+import co.paralleluniverse.fibers.Suspendable
+import com.template.contracts.UserContract
+import com.template.states.UserState
+import net.corda.core.contracts.Command
+import net.corda.core.contracts.UniqueIdentifier
+import net.corda.core.contracts.requireThat
+import net.corda.core.flows.*
+import net.corda.core.transactions.SignedTransaction
+import net.corda.core.transactions.TransactionBuilder
+
+@InitiatingFlow
+@StartableByRPC
+class VerifyUserFlow2(private val id: String) : UserBaseFlow() {
+    @Suspendable
+    override fun call(): SignedTransaction {
+        val transaction = transaction()
+        val signedTransaction = verifyAndSign(transaction)
+        val sessions = (output().participants - ourIdentity).map { initiateFlow(it) }.toSet().toList()
+        val transactionSignedByAllParties = collectSignature(signedTransaction, sessions)
+        return recordTransaction(transactionSignedByAllParties, sessions)
+    }
+
+    private fun transaction(): TransactionBuilder {
+        val notary = firstNotary
+
+        val refState = getUserByLinearId(UniqueIdentifier.fromString(id))
+
+        val verifyCommand =
+                Command(UserContract.Commands.Verify(), output().participants.map { it.owningKey })
+        val builder = TransactionBuilder(notary = notary)
+        builder.addInputState(refState)
+        builder.addOutputState(output(), UserContract.USER_CONTRACT_ID)
+        builder.addCommand(verifyCommand)
+        return builder
+    }
+
+    private fun output(): UserState {
+        return getUserByLinearId(UniqueIdentifier.fromString(id)).state.data.verify(listStringToParty(allParties()))
+    }
+
+}
+
+@InitiatedBy(VerifyUserFlow2::class)
+class VerifyUserFlow2Responder(val flowSession: FlowSession) : UserBaseFlow() {
+
+    @Suspendable
+    override fun call(): SignedTransaction {
+        val signTransactionFlow = object : SignTransactionFlow(flowSession) {
+            override fun checkTransaction(stx: SignedTransaction) = requireThat {
+                val output = stx.tx.outputs.single().data
+                "This must be an user transaction" using (output is UserState)
+            }
+        }
+        val signedTransaction = subFlow(signTransactionFlow)
+        return subFlow(ReceiveFinalityFlow(otherSideSession = flowSession, expectedTxId = signedTransaction.id))
+    }
+}
