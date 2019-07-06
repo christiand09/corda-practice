0488384254d2b654cf047e94ff15427b6683096d
Lance Navarro <lancegbn@gmail.com>
2019-07-07 00:01:53 +0800

    remove KYC files

--- contracts/src/main/kotlin/com/template/states/UserState.kt
+++ contracts/src/main/kotlin/com/template/states/UserState.kt
@@ -5,6 +5,7 @@ import net.corda.core.contracts.BelongsToContract
 import net.corda.core.contracts.LinearState
 import net.corda.core.contracts.UniqueIdentifier
 import net.corda.core.identity.Party
+import net.corda.core.serialization.CordaSerializable
 
 
 @BelongsToContract(UserContract::class)
@@ -21,8 +22,8 @@ data class UserState(
 
 }
 
-//@CordaSerializable
-//data class Name(
-//        val firstName: String,
-//        val lastName: String
-//)
+@CordaSerializable
+data class Name(
+        val firstName: String,
+        val lastName: String
+)
